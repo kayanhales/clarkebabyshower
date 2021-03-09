db4b6926589877ffe32d92d2a7a3eb37f120a770
kayanhales <daybreakbliss@gmail.com>
2021-03-08 21:58:07 -0500

    Initial commit

---
+++ source/index.blade.php
@@ -0,0 +1,27 @@
+@extends('_layouts.master')
+
+@section('content')
+    
+
+    <p><center><a href="https://ibb.co/Nnfzj4t"><img src="https://i.ibb.co/S7HpnL5/40thweddinganniversary-With-NOTHING.png" alt="40thweddinganniversary-With-NOTHING" border="0"></a></center></p>
+    <h1>YouTube</h1>
+    <p><a href="https://youtu.be/BfX3-uG_Kao">https://youtu.be/BfX3-uG_Kao</a></p>
+    <h1>Zoom</h1>
+    <p>Join with the Zoom App: <a href="https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09">https://us02web.zoom.us/j/82372576873?pwd=M0crNkR0L3ZUQ3V2c1AzcWJvUlZQdz09</a></p>
+    <p>Meeting ID: 823 7257 6873, Passcode: 511173</p>
+    <p>Or Dial-in: +19292056099, Meeting ID: 82372576873#, Passcode: *511173#</p>
+    <h1>Program</h1>
+    <p><small> Start time: 4PM. End Time: 6PM</small></br><small> <b>Master of Ceremonies: Bro. Michael Thompson</b></small>
+        <ol>
+            <li>Arrival and Welcome of Guests
+            <li>Entrance of Celebrants</li>
+            <li>Devotion</li>
+            <li>Welcome Remarks</li>
+            <li>Love Story of the Celebrants</li>
+            <li>Cake Cutting</li>
+            <li>Dinner/Toasts</li>
+            <li>Thank You Message from the Celebrants</li>
+        <ol>
+    </p>
+    
+@endsection
