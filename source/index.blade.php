4a66dbd1a05e6d01855fc66b065991a5ebd5bd44
kayanhales <daybreakbliss@gmail.com>
2021-03-08 22:50:25 -0500

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,19 +3,22 @@
 @section('content')
     
 
-    <p><center><a href="https://ibb.co/Nnfzj4t"><img src="https://i.ibb.co/S7HpnL5/40thweddinganniversary-With-NOTHING.png" alt="40thweddinganniversary-With-NOTHING" border="0"></a></center></p>
+    <p><center><a href="https://images.pexels.com/photos/2814808/pexels-photo-2814808.jpeg?cs=srgb&dl=pexels-fidel-hajj-2814808.jpg&fm=jpg" alt="wedding" border="0"></a></center></p>
+    <h1>Program</h1>
+    <p>Start time: 3:30PM CST.
+    </p>
     <h1>Watch on YouTube</h1>
     <p><a href="https://youtu.be/ONGIBBQKfjo">https://youtu.be/ONGIBBQKfjo</a></p>
     <h1>Watch on Zoom</h1>
     <p>
 Join Zoom Meeting</br>
-https://us02web.zoom.us/j/81754502434?pwd=VVBFY212d1gxY2huZWc0ajNIZ0NjQT09</br>
+https://us02web.zoom.us/j/81754502434?pwd=VVBFY212d1gxY2huZWc0ajNIZ0NjQT09</br></br>
 
 Meeting ID: 817 5450 2434</br>
-Passcode: 574287</br>
+Passcode: 574287</br></br>
 One tap mobile</br>
 +19292056099,,81754502434#,,,,*574287# US (New York)</br>
-+13017158592,,81754502434#,,,,*574287# US (Washington DC)</br>
++13017158592,,81754502434#,,,,*574287# US (Washington DC)</br></br>
 
 Dial by your location</br>
         +1 929 205 6099 US (New York)</br>
@@ -23,13 +26,11 @@ Dial by your location</br>
         +1 312 626 6799 US (Chicago)</br>
         +1 669 900 6833 US (San Jose)</br>
         +1 253 215 8782 US (Tacoma)</br>
-        +1 346 248 7799 US (Houston)</br>
+        +1 346 248 7799 US (Houston)</br></br>
 Meeting ID: 817 5450 2434</br>
 Passcode: 574287</br>
 Find your local number: https://us02web.zoom.us/u/kb5XV1SmG
 </p>
-    <h1>Program</h1>
-    <p>Start time: 3:30PM CST.
-    </p>
+    
     
 @endsection
