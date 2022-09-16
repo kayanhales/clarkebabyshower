93c0309d376ce76069c72db77a3f6c242ed98c96
kayanhales <daybreakbliss@gmail.com>
2022-09-15 21:35:01 -0400

    Changed titles

--- source/_partials/head/meta.blade.php
+++ source/_partials/head/meta.blade.php
@@ -2,17 +2,17 @@
 <link rel="canonical" href="{{ $page->getUrl() }}">
 <meta name="referrer" content="no-referrer-when-downgrade">
 <!--<meta name="author" content="{{ $page->owner->name }}">-->
-<meta name="author" content="Kayan Hales Clarke">
+<meta name="author" content="Kayan Hales-Clarke">
 <!--<meta name="description" content="{!! $page->excerpt() ?: $page->site->description !!}">-->
-<meta name="description" content="Rachel and Joshua's Wedding">
+<meta name="description" content="Shane and Kayan's Baby Shower">
 <!--<meta property="og:title" content="{{ $page->title ?: $page->site->title }}">-->
-<meta property="og:title" content="Rachel and Joshua's Wedding">
+<meta property="og:title" content="Shane and Kayan's Baby Shower">
 <!--<meta property="og:description" content="{!! $page->excerpt() ?: $page->site->description !!}">-->
-<meta property="og:description" content="Rachel and Joshua's Wedding">
+<meta property="og:description" content="Shane and Kayan's Baby Shower">
 <meta property="og:image" content="{{ $page->image ?: media($page->site->image) }}">
 <meta property="og:type" content="{{ $page->isPost ? 'article' : 'website' }}">
 <!--<meta property="og:site_name" content="{{ $page->site->title }}">-->
-<meta property="og:site_name" content="Rachel and Joshua's Wedding">
+<meta property="og:site_name" content="Shane and Kayan's Baby Shower">
 <meta property="og:url" content="{{ $page->getUrl() }}">
 
 <meta name="twitter:title" content="{{ $page->title ?: $page->site->title }}">
