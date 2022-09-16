07c4448b72791b7320d9999f93b689087f7846ad
kayanhales <daybreakbliss@gmail.com>
2022-09-16 13:41:06 -0400

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,24 +11,30 @@
     <h1>Watch on Zoom</h1>
     <p>
 Join Zoom Meeting</br>
-<a href="https://us02web.zoom.us/j/81754502434?pwd=VVBFY212d1gxY2huZWc0ajNIZ0NjQT09">https://us02web.zoom.us/j/81754502434?pwd=VVBFY212d1gxY2huZWc0ajNIZ0NjQT09</a></br></br>
+https://us02web.zoom.us/j/82713190283?pwd=RUJWbWRJLzBSM09GLzliMExtTm0vQT09</br></br>
 
-Meeting ID: 817 5450 2434</br>
-Passcode: 574287</br></br>
+Meeting ID: 827 1319 0283</br>
+Passcode: 557676</br>
 One tap mobile</br>
-+19292056099,,81754502434#,,,,*574287# US (New York)</br>
-+13017158592,,81754502434#,,,,*574287# US (Washington DC)</br></br>
++13126266799,,82713190283#,,,,*557676# US (Chicago)</br>
++16469313860,,82713190283#,,,,*557676# US</br></br>
 
 Dial by your location</br>
+        +1 312 626 6799 US (Chicago)</br>
+        +1 646 931 3860 US</br>
         +1 929 205 6099 US (New York)</br>
         +1 301 715 8592 US (Washington DC)</br>
-        +1 312 626 6799 US (Chicago)</br>
+        +1 309 205 3325 US</br>
+        +1 669 444 9171 US</br>
         +1 669 900 6833 US (San Jose)</br>
+        +1 719 359 4580 US</br>
         +1 253 215 8782 US (Tacoma)</br>
-        +1 346 248 7799 US (Houston)</br></br>
-Meeting ID: 817 5450 2434</br>
-Passcode: 574287</br>
-Find your local number: <a href="https://us02web.zoom.us/u/kb5XV1SmG">https://us02web.zoom.us/u/kb5XV1SmG</a>
+        +1 346 248 7799 US (Houston)</br>
+        +1 386 347 5053 US</br>
+        +1 564 217 2000 US</br></br>
+Meeting ID: 827 1319 0283</br>
+Passcode: 557676</br>
+Find your local number: <a href="https://us02web.zoom.us/u/kjxTL1QNa">HERE</a>
 </p>
     
     
